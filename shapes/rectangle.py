5af62c5dd724bc9c9fa7051ce82c6fe5a95f3279
MnFeN <self.mail.address@gmail.com>
2024-06-12 19:13:37 -0700

    Update rectangle.py

--- shapes/rectangle.py
+++ shapes/rectangle.py
@@ -6,8 +6,7 @@ def from_point_coords(x1: float, y1: float, x2: float, y2: float, half_width: fl
     dy = y2 - y1
 
     if (dx == 0 and dy == 0):
-        print("The 2 given coordinations are the same.")
-        return None
+        raise ValueError("The 2 given coordinations are the same.")
     
     gcd_dxy = 1
     if (dx_4 := dx * 4) == int(dx_4) and (dy_4 := dy * 4) == int(dy_4):
@@ -18,10 +17,10 @@ def from_point_coords(x1: float, y1: float, x2: float, y2: float, half_width: fl
 
     if (dx == 0):
         result = (f"|x - {x_avr}| <= {half_width} ∧ "
-                f"|y - {y_avr}| <= {abs(dy):.3f} / 2")
+                  f"|y - {y_avr}| <= {abs(dy):.3f} / 2")
     elif (dy == 0):
         result = (f"|y - {y_avr}| <= {half_width} ∧ "
-                f"|x - {x_avr}| <= {abs(dx):.3f} / 2")
+                  f"|x - {x_avr}| <= {abs(dx):.3f} / 2")
     else:
         norm_dx = dx / gcd_dxy
         norm_dy = dy / gcd_dxy
@@ -30,7 +29,7 @@ def from_point_coords(x1: float, y1: float, x2: float, y2: float, half_width: fl
         norm_dy = round(norm_dy, 3)
 
         result = (f"|{norm_dy}(x - {x_avr}) - {norm_dx}(y - {y_avr})| <= {half_width} * √ {norm_dsquare} ∧ "
-                    f"|{norm_dx}(x - {x_avr}) + {norm_dy}(y - {y_avr})| <= {norm_dsquare} * {gcd_dxy} / 2")
+                  f"|{norm_dx}(x - {x_avr}) + {norm_dy}(y - {y_avr})| <= {norm_dsquare} * {gcd_dxy} / 2")
 
     result = result.replace("- -", "+ ").replace("+ -", "- ")
     return result
@@ -40,7 +39,7 @@ def from_names(name1: str, name2: str, half_width: str) -> str:
     y_avr = f"(y({name1}) + y({name2})) / 2" 
     dx = f"(x({name2}) - x({name1}))"
     dy = f"(y({name2}) - y({name1}))"
-    d_square = f"( (x({name2}) - x({name1})) ^ 2 + (y({name2}) - y({name1})) ^ 2)"
+    d_square = f"((x({name2}) - x({name1})) ^ 2 + (y({name2}) - y({name1})) ^ 2)"
     result = (f"|{dy}(x - {x_avr}) - {dx}(y - {y_avr})| <= {half_width} * √ {d_square} ∧ "
-                f"|{dx}(x - {x_avr}) + {dy}(y - {y_avr})| <= {d_square} / 2")
+              f"|{dx}(x - {x_avr}) + {dy}(y - {y_avr})| <= {d_square} / 2")
     return result
